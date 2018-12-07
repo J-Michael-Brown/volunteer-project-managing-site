fcb8778e033a8a4cfe98bb2e0f00f2b1766365d8
John Michael Brown <jmichaelbrown132737@gmail.com>
2018-12-07 12:29:50 -0800

    begin site integration testing

--- app.rb
+++ app.rb
@@ -9,5 +9,32 @@ require('pg')
 DB = PG.connect({:dbname => 'volunteer_tracker'})
 
 get ('/') do
+  @projects = Project.all
   erb(:index)
+end
+
+patch ('/') do
+  title = params.fetch("title")
+  new_project = Project.new({:title => title})
+  new_project.save
+  @projects = Project.all
+  erb(:index)
+end
+
+get ('/project/:id') do
+  @project = Project.find(params[:id])
+  erb(:project)
+end
+
+get ('/project/:id/edit') do
+  @project = Project.find(params[:id])
+  erb(:project_edit)
+end
+
+patch ('/project/:id/edit') do
+  @project = Project.find(params[:id])
+  new_title = params.fetch(:title)
+  @project.update({:title => new_title})
+
+  erb(:project_edit)
 end
